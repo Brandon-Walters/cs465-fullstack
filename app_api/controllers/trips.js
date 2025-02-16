25e7d75caaab22f1a0ab04290ac5efe608ab6857
Brandon Walters <brandon.walters@snhu.edu>
2025-02-15 19:36:36 -0800

    Module 5 completed baseline

---
+++ app_api/controllers/trips.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Trip = require("../models/travlr"); // Register model
+const Model = mongoose.model("trips");
+// GET: /trips - lists all the trips
+// Regardless of outcome, response must include HTML status code
+// and JSON messager  to the requesting client
+const tripsList = async (req, res) => {
+  const q = await Model.find({}).exec();
+
+  if (!q) {
+    // Database returned no data
+    return res.status(404).json(err);
+  } else {
+    // Return resulting trip list
+    return res.status(200).json(q);
+  }
+};
+
+// GET: /trips/:tripCode - lists a single trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsFindByCode = async (req, res) => {
+  const q = await Model.find({ code: req.params.tripCode }) // Return a single record
+    .exec();
+
+  if (!q) {
+    return res.status(404).json(err);
+  } else {
+    return res.status(200).json(q);
+  }
+};
+
+module.exports = {
+  tripsList,
+  tripsFindByCode,
+};
